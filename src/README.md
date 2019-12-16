8913091f28e9ea6cbb404a7c68721749c3a1aebe
JT McHorse <jt.mchorse@glg.com>
2019-12-16 13:56:50 -0800

    Commit
    Plumbed index.html with js and css and made readme

---
+++ src/README.md
@@ -0,0 +1,1 @@
+### TeamTreehouse React Scorebord repo
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const subtitle = React.createElement(
+  "h2",
+  { id: "sub-title", title: "helper", className: "bar" },
+  "Sub title element"
+);
+
 const title = React.createElement(
   "h1",
-  { id: "main-title", title: "this is a title", class: "foo" },
-  "My First React Element!"
+  { id: "main-title", title: "this is a title", className: "foo" },
+  "My First React Element! "
 );
+
+ReactDOM.render([title, subtitle], document.getElementById("root"));
